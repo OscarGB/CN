6f29d652936efd535fa6ae6aaeb4f7295ac4bbd7
OscarGB <oscar.gomez.borz@gmail.com>
2017-01-31 13:14:38 +0100

    final clase

---
+++ posicional.m
@@ -0,0 +1,27 @@
+% funcion cambio de base
+
+function [a,y] = posicional(x,b) % a es el return, x y b es el input
+    
+    j = 1; % En matlab los vectores empiezan en 1
+    
+    a(j) = mod(x,b);
+    r = floor(x/b);
+    
+    while r > 0
+       
+        j = j + 1;
+        a(j) = mod(r,b);
+        r = floor(r/b);        
+        
+    end
+    
+    l = length(a);
+    y = 0;
+    for k=1:l
+        
+        y = y + a(k)*b^(k-1);
+    
+    end
+    
+    
+end

925c0a0c859bb73eba382cd13f6171cab49a20fd
OscarGB <oscar.gomez.borz@gmail.com>
2017-04-24 18:08:33 +0200

    Newton varios puntos

--- Oscar/InterNewton.m
+++ Oscar/InterNewton.m
@@ -1,30 +1,39 @@
 function p = InterNewton(x, y, evaluation)
-  [a,m] = size(x);
-  [a,b] = size(y);
+  m = length(x);
+  b = length(y);
+  
   if(m ~= b)
     error('Los vectores tienen que ser iguales');
   end
    
   M = zeros(m, m);
   i = 2;
+  
   for size = 1:m
     M(size, 1) = y(size);   
   end
   
   for columns = 2:m
+      
     for rows = i:m
       M(rows, columns) = (M(rows, columns-1) - M(rows-1, columns-1)) / (x(rows) - x(rows-i+1));
     end
     i = i +1;
+    
   end
   
-  p = 0;
-  g = 1;
-  for i = 1:m
-    p = p + g * M(i,i)
-    g = g * (evaluation - x(i));
+  ev = length(evaluation);
+  p = zeros(1,ev);
+  
+  for j = 1:ev
+      g = 1;
+      
+      for i = 1:m
+        p(j) = p(j) + g * M(i,i);
+        g = g * (evaluation(j) - x(i));
+      end
+      
   end
-
-
-
+  
+  return
 end
